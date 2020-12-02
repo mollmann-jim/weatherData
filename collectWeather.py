475887e780f3ae47d326a6ca30e8bd4e1507965c
Jim Mollmann <jim.mollmann@gmail.com>
2020-12-02 14:46:26 -0500

    initial commit

---
+++ collectWeather.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+from html.parser import HTMLParser
+import requests
+import datetime as dt
+import sqlite3
+from dateutil.tz import tz
+
+
+RDU='https://w1.weather.gov/data/obhistory/KRDU.html'
+MYR='https://w1.weather.gov/data/obhistory/KMYR.html'
+DBname = '/home/jim/tools/weather.data/weather.sql'
+
+class DB:
+    def __init__(self, site):
+        self.db = sqlite3.connect(DBname)
+        self.db.row_factory = sqlite3.Row
+        self.c = self.db.cursor()
+        self.table = site
+        
+        ##self.c.execute('DROP TABLE IF EXISTS ' + self.table)
+
+        create = 'CREATE TABLE IF NOT EXISTS ' + self.table + '(\n' +\
+            ' timestamp        DATETIME DEFAULT CURRENT_TIMESTAMP PRIMARY KEY,\n' +\
+            ' wind             INTEGER, \n' +\
+            ' winddirection    TEXT, \n' +\
+            ' gust             INTEGER, \n' +\
+            ' visibility       REAL, \n' +\
+            ' sky              TEXT, \n' +\
+            ' clouds           TEXT, \n' +\
+            ' temperature      INTEGER, \n' +\
+            ' dewpoint         INTEGER, \n' +\
+            ' temp6hrMax       INTEGER DEFAULT NULL, \n' +\
+            ' temp6hrMin       INTEGER DEFAULT NULL, \n' +\
+            ' humidity         INTEGER, \n' +\
+            ' windchill        INTEGER DEFAULT NULL, \n' +\
+            ' heatindex        INTEGER DEFAULT NULL, \n' +\
+            ' altimiterIn      REAL, \n' +\
+            ' altimiterMb      REAL, \n' +\
+            ' precipitation1hr REAL DEFAULT NULL, \n' +\
+            ' precipitation3hr REAL DEFAULT NULL, \n' +\
+            ' precipitation6hr REAL DEFAULT NULL  \n' +\
+            ' )'
+        self.c.execute(create)
+        self.db.commit()
+
+    def Insert(self, data):
+        DBdata = {}
+        insert = 'INSERT OR REPLACE INTO ' + self.table + ' (timestamp, wind, winddirection, ' +\
+            'gust, visibility, sky, clouds, temperature, dewpoint, temp6hrMax, ' +\
+            'temp6hrMin, humidity, windchill, heatindex, altimiterIn, altimiterMb, ' +\
+            'precipitation1hr, precipitation3hr, precipitation6hr )' +\
+            'VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
+        columns = ['timestamp', 'wind', 'winddirection',  \
+            'gust', 'visibility', 'sky', 'clouds', 'temperature', 'dewpoint', 'temp6hrMax',  \
+            'temp6hrMin', 'humidity', 'windchill', 'heatindex', 'altimiterIn', 'altimiterMb', \
+            'precipitation1hr', 'precipitation3hr', 'precipitation6hr']
+        for key in data:
+            DBdata[key] = data[key]
+        winds = data['wind'].split(' ')
+        DBdata['gust'] = None
+        DBdata['winddirection'] = winds[0]
+        if winds[0] == 'Calm':
+            DBdata['wind'] = 0
+        else:
+            DBdata['wind'] = winds[1]
+        if winds[0] == 'G':
+            DBdata['gust'] = winds[3]
+        DBdata['humidity'] = DBdata['humidity'].replace('%', '')
+        if DBdata['windchill'] == 'NA' : DBdata['windchill'] = None
+        if DBdata['heatindex'] == 'NA' : DBdata['heatindex'] = None
+        now = dt.datetime.now()
+        hh, mm = DBdata['time'].split(':')
+        year = now.year
+        month = now.month
+        day = int(DBdata['day'])
+        if now.day < day:
+            month = month - 1
+            if month < 1:
+                month = 12
+                year -= 1
+        timestamp = dt.datetime(year, month, day, int(hh), int(mm), \
+                                          tzinfo = tz.gettz('America/New_York'))
+        DBdata['timestamp'] = timestamp
+        values = []
+        for col in columns:
+            if DBdata[col] == '' : DBdata[col] = None
+            values.append(DBdata[col])
+        self.c.execute(insert, values)
+        self.db.commit()
+        
+
+class MyHTMLParser(HTMLParser, DB):
+    def __init__(self, site):
+        self.i = 0
+        HTMLParser.__init__(self)
+        DB.__init__(self, site)
+        self.line = []
+        self.item = ''
+        self.myInteresting = False
+        self.tdOpen = False
+        self.Cols = ['day', 'time', 'wind', 'visibility', 'sky', 'clouds', 'temperature', 'dewpoint', \
+                     'temp6hrMax', 'temp6hrMin', 'humidity', 'windchill', 'heatindex', 'altimiterIn', \
+                     'altimiterMb', 'precipitation1hr', 'precipitation3hr', 'precipitation6hr']
+        
+    def handle_starttag(self, tag, attrs):
+        if tag == 'tr':
+            attr = {}
+            for name, value in attrs:
+                attr[name] = value
+            if attr.get('valign', 'no') == 'top' and attr.get('align', 'no') == 'center':
+                self.i += 1
+                self.myInteresting = True
+                self.line = []
+        elif tag == 'td':
+            self.item = ''
+            self.tdOpen = True
+
+    def handle_endtag(self, tag):
+        if tag == 'tr':
+            if self.myInteresting:
+                #print('*** DATA ***', self.line)
+                myInfo = dict(zip(self.Cols, self.line))
+                #print(myInfo)
+                self.Insert(myInfo)
+                pass
+            self.myInteresting = False
+        elif tag == 'td':
+            self.tdOpen = False
+            if self.myInteresting:
+                self.line.append(self.item)
+        pass
+
+    def handle_data(self, data):
+        if self.myInteresting:
+            if self.tdOpen:
+                self.item = data
+        pass
+
+RDUparser = MyHTMLParser('RDU')
+
+r = requests.get(RDU)
+data = r.text
+lines = data.split('\r')
+lines.pop(0)
+data = ''.join(lines)
+data = data.replace('\n','')
+#print(data)
+RDUparser.feed(data)
+
+MYRparser = MyHTMLParser('MYR')
+
+r = requests.get(MYR)
+data = r.text
+lines = data.split('\r')
+lines.pop(0)
+data = ''.join(lines)
+data = data.replace('\n','')
+#print(data)
+MYRparser.feed(data)
---
+++ reportWeather.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+import datetime as dt
+import sqlite3
+from dateutil.tz import tz
+from sys import path
+path.append('/home/jim/tools/')
+from shared import getTimeInterval
+
+DBname = '/home/jim/tools/weather.data/weather.sql'
+table = 'weather'
+
+'''
+def getPeriod(which, year = None):
+    now   = dt.datetime.now(tz = tz.gettz('America/New_York'))
+    if which == 'Today':
+        end   = now.replace(hour = 23, minute = 59, second = 0, microsecond = 0)
+        start = now.replace(hour = 0, minute = 0, second =0, microsecond = 0) 
+    elif which == 'Prev7days':
+        end   = now.replace(hour = 23, minute = 59, second = 0, microsecond = 0) - \
+            dt.timedelta(days = 1)
+        start = now.replace(hour = 0, minute = 0, second =0, microsecond = 0) - \
+            dt.timedelta(days = 7)
+    elif which == 'This Week':
+        end   = now.replace(hour = 23, minute = 59, second = 0, microsecond = 0) - \
+            dt.timedelta(days = 1)
+        start = now.replace(hour = 0, minute = 0, second = 0, microsecond = 0) - \
+            dt.timedelta(days = now.weekday())
+    elif which == 'Last Week':
+        end   = now.replace(hour = 23, minute = 59, second = 0, microsecond = 0) - \
+            dt.timedelta(days = 1 + now.weekday())
+        start = now.replace(hour = 0, minute = 0, second = 0, microsecond = 0) - \
+            dt.timedelta(days = 7 + now.weekday())
+    elif which == 'This Month':
+        end   = dt.datetime(now.year, now.month + 1, 1, tzinfo = tz.gettz('America/New_York')) -\
+                            dt.timedelta(seconds = 1) 
+        start = now.replace(day = 1, hour = 0, minute = 0, second = 0, microsecond = 0)
+    elif which == 'Last Month':
+        end   = dt.datetime(now.year, now.month, 1, tzinfo = tz.gettz('America/New_York')) - \
+                            dt.timedelta(seconds = 1)
+        month = now.month - 1
+        if month < 1: month = 12
+        start = dt.datetime(now.year, month, 1, tzinfo = tz.gettz('America/New_York'))
+    elif which == 'Year':
+        year = int(year)
+        end   = dt.datetime(year = year + 1, month = 1, day = 1, tzinfo = tz.gettz('America/New_York')) - \
+            dt.timedelta(seconds = 1)
+        start =  dt.datetime(year = year, month = 1, day = 1, tzinfo = tz.gettz('America/New_York'))
+    else:
+        print('getPeriod: ', which, 'not implemented')
+        start = end = None
+    return start, end
+'''
+
+def fmtLine(tag, row):
+    line = tag + ': (none)'
+    if row['minT']:
+        period =  '{:>10s}'.format(tag)
+        minT   = ' {:>5d}'.format(row['minT'])
+        maxT   = ' {:>5d}'.format(row['maxT'])
+        avgT   = ' {:>5.1f}'.format(row['avgT'])
+        minD   = ' {:>5d}'.format(row['minD'])
+        maxD   = ' {:>5d}'.format(row['maxD'])
+        avgD   = ' {:>5.1f}'.format(row['avgD'])
+        wind   = ' {:>5.1f}'.format(row['avgW'])
+        rain   = ' {:>5.2f}'.format(row['rain']).replace(' 0.00','    0')
+        line = period + minT + maxT + avgT + minD + maxD + avgD + wind + rain
+    return line
+                                
+
+def printHeader():
+    # period min/max/avg(temp) min/max/avg(dew) avg(wind) sum(precip)
+    #      2020/07/02 mmmmm MMMMM aaaaa mmmmm MMMMM aaaaa wwwww rrrrr
+    print('')
+    print('             Min   Max   Avg   Min   Max   Avg   Avg Total')
+    print('            Temp  Temp  Temp DewPt DewPt DewPt  Wind  Rain')
+
+def getYears(c, site):
+    select_min_yr = 'SELECT min(timestamp) AS min FROM ' + site + ';'
+    c.execute(select_min_yr)
+    min = c.fetchone()
+    first = dt.datetime.strptime(min['min'], '%Y-%m-%d %H:%M:%S%z')
+    select_max_yr = 'SELECT max(timestamp) AS max FROM ' + site + ';'
+    c.execute(select_max_yr)
+    max = c.fetchone()
+    last = dt.datetime.strptime(max['max'], '%Y-%m-%d %H:%M:%S%z')
+    return first, last
+
+def makeSection(c, site, title, byDay = False, byMonth = False, year = None):
+    start, end, name = getTimeInterval.getPeriod(title, year = year)
+    selectFields = 'SELECT date(timestamp) as date, ' +\
+        'MIN(temperature) AS minT, MAX(temperature) AS maxT, AVG(temperature) AS avgT, ' +\
+        'MIN(dewpoint) AS minD, MAX(dewpoint) AS maxD, AVG(dewpoint) AS avgD, ' +\
+        'AVG(wind) AS avgW, TOTAL(precipitation1hr) AS rain ' +\
+        'FROM ' + site + ' WHERE timestamp >= ? AND timestamp <= ?'
+    select = selectFields + ' ;'
+    # sqlite date(timestamp) returns the UTC date
+    #selectByDay = selectFields + ' GROUP BY date(timestamp) ORDER BY date(timestamp) DESC;'
+    selectByDay   = selectFields + ' GROUP BY substr(timestamp,1,10) ORDER BY timestamp DESC;'
+    selectByMonth = selectFields + ' GROUP BY substr(timestamp,1, 7) ORDER BY timestamp DESC;'
+    #print(title, start, end)
+    if byDay:
+        c.execute(selectByDay, (start, end))
+    elif byMonth:
+        c.execute(selectByMonth, (start, end))
+    else:
+        c.execute(select, (start, end))
+    result = c.fetchall()
+    #printHeader()
+    for record in result:
+        if byDay:
+            print(fmtLine(record['date'], record))
+        elif byMonth:
+            print(fmtLine(record['date'][0:7], record))
+        else:
+            print(fmtLine(name, record))
+
+def makeReport(c, site):
+    first, last = getYears(c, site)
+    print('---------------------------', site, '----------------------------')
+    printHeader()
+    makeSection(c, site, 'Today')
+    makeSection(c, site, 'Prev7days', byDay = True)
+
+    printHeader()
+    for period in ['This Week', 'Last Week', 'This Month', 'Last Month']:
+        makeSection(c, site,  period)
+    for period in ['This Month', 'Last Month']:
+        printHeader()
+        for year in range(last.year, first.year - 1, -1):
+            makeSection(c, site , period, year = year)
+    printHeader()
+    makeSection(c, site, 'YearByMonth', byMonth = True)
+    makeSection(c, site, 'LastYear')
+    printHeader()
+    for year in range(last.year, first.year - 1, -1):
+        makeSection(c, site, 'Year', year = year)
+    print('')
+    makeSection(c, site,  'All')
+
+def main():
+    db = sqlite3.connect(DBname)
+    db.row_factory = sqlite3.Row
+    c = db.cursor()
+    #db.set_trace_callback(print)
+
+    makeReport(c, 'RDU')
+    makeReport(c, 'MYR')
+
+if __name__ == '__main__':
+  main()
